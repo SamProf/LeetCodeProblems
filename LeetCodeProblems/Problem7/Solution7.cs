c9eba2a22a6f74c57a669954490d450af0b3926a
Vladimir Samoilenko <wsamprof@gmail.com>
2018-04-09 17:54:19 +0200

    Promlem 7

---
+++ LeetCodeProblems/Problem7/Solution7.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.Problem7
+{
+    public class Solution7
+    {
+        public int Reverse(int x)
+        {
+            if (x < -1 * int.MaxValue)
+            {
+                return 0;
+            }
+
+            var r = 0;
+            var x2 = Math.Abs(x);
+            while (x2 != 0)
+            {
+                if (r > (int.MaxValue - (x2 % 10)) / 10)
+                {
+                    return 0;
+                }
+                r = r * 10 + (x2 % 10);
+                x2 /= 10;
+            }
+
+            r *= Math.Sign(x);
+            return r;
+        }
+    }
+}
---
+++ LeetCodeProblems/Problem7/Tests7.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LeetCodeProblems.Problem7
+{
+    [TestFixture]
+    public class Tests7
+    {
+        [Test]
+        public void Test1()
+        {
+            var solver = new Solution7();
+            Assert.AreEqual(2147483641, solver.Reverse(1463847412));
+            Assert.AreEqual(0, solver.Reverse(1534236469));
+            Assert.AreEqual(0, solver.Reverse(-2147483648));
+            Assert.AreEqual(321, solver.Reverse(123));
+            Assert.AreEqual(-321, solver.Reverse(-123));
+            Assert.AreEqual(21, solver.Reverse(120));
+        }
+    }
+}
