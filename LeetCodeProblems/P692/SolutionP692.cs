06deed062225281661e400258e07606bedeec96f
SamProf <wsamprof@gmail.com>
2020-04-10 14:55:28 +0200

    P692

---
+++ LeetCodeProblems/P692/SolutionP692.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCodeProblems.P692
+{
+    public class SolutionP692
+    {
+        public IList<string> TopKFrequent(string[] words, int k)
+        {
+            var dic = new Dictionary<string, int>();
+            for (int i = 0, len = words.Length; i < len; i++)
+            {
+                int c;
+                if (!dic.TryGetValue(words[i], out c))
+                {
+                    c = 0;
+                }
+
+                dic[words[i]] = c + 1;
+            }
+
+            var set = new SortedSet<KeyValuePair<string, int>>(Comparer<KeyValuePair<string, int>>.Create((v1, v2) =>
+            {
+                return (v2.Value == v1.Value) ? v1.Key.CompareTo(v2.Key) : v2.Value - v1.Value;
+            }));
+
+            foreach (var i in dic)
+            {
+                set.Add(i);
+                if (set.Count > k)
+                {
+                    set.Remove(set.Max);
+                }
+            }
+            return set.Select(i=>i.Key).ToList();
+        }
+
+
+        public IList<string> TopKFrequent_Old(string[] words, int k)
+        {
+            return words
+                .GroupBy(i => i)
+                .ToDictionary(i => i.Key, i => i.Count())
+                .OrderByDescending(i => i.Value)
+                .ThenBy(i => i.Key)
+                .Take(k)
+                .Select(i => i.Key)
+                .ToList();
+        }
+    }
+}
---
+++ LeetCodeProblems/P692/TestsP692.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LeetCodeProblems.P692
+{
+    [TestFixture]
+    public class TestsP692
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new SolutionP692();
+            var result = s.TopKFrequent(new[] {"i", "love", "leetcode", "i", "love", "coding"}, 2);
+            Console.WriteLine(string.Join(" ", result));
+            Assert.AreEqual(result, new string[] {"i", "love"});
+        }
+
+
+        [Test]
+        public void Example2()
+        {
+            var s = new SolutionP692();
+            var result = s.TopKFrequent(new[] {"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"},
+                4);
+            Console.WriteLine(string.Join(" ", result));
+            Assert.AreEqual(result, new string[] {"the", "is", "sunny", "day"});
+        }
+    }
+}
