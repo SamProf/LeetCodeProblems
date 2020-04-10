ba2ea17028601ecb358bbbfeb574afe2505e609e
SamProf <wsamprof@gmail.com>
2020-04-10 16:50:45 +0200

    IQ542597

---
+++ LeetCodeProblems/IQ542597/SolutionIQ542597.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace LeetCodeProblems.IQ542597
+{
+    public class SolutionIQ542597
+    {
+        public string[] Solve(int k, string[] keywords, string[] reviews)
+        {
+            var regex = new Regex("\\w+", RegexOptions.Compiled);
+            var dic = new Dictionary<string, int>(StringComparer.InvariantCultureIgnoreCase);
+            for (int i = 0; i < keywords.Length; i++)
+            {
+                dic.Add(keywords[i], 0);
+            }
+
+            foreach (var review in reviews)
+            {
+                var hs = new HashSet<string>(StringComparer.InvariantCultureIgnoreCase);
+                foreach (Match match in regex.Matches(review))
+                {
+                    var c = 0;
+                    if (!dic.TryGetValue(match.Value, out c) || hs.Contains(match.Value))
+                    {
+                        continue;
+                    }
+
+                    dic[match.Value] = c + 1;
+                    hs.Add(match.Value);
+                }
+            }
+            var ss = new SortedSet<KeyValuePair<string, int>>(Comparer<KeyValuePair<string, int>>.Create((v1, v2) =>
+            {
+                return v1.Value == v2.Value ? v1.Key.CompareTo(v2.Key) : v2.Value - v1.Value;
+            }));
+
+            foreach (var item in dic)
+            {
+                ss.Add(item);
+                if (ss.Count > k)
+                {
+                    ss.Remove(ss.Max);
+                }
+            }
+
+            return ss.Select(i => i.Key).ToArray();
+        }
+    }
+}
---
+++ LeetCodeProblems/IQ542597/TestIQ542597.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LeetCodeProblems.IQ542597
+{
+    [TestFixture]
+    public class TestIQ542597
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new SolutionIQ542597();
+            var r = s.Solve(2, new[] {"anacell", "cetracular", "betacellular"}, new[]
+            {
+                "Anacell provides the best services in the city",
+                "betacellular has awesome services",
+                "Best services provided by anacell, everyone should use anacell",
+            });
+            Console.WriteLine(string.Join(" ", r));
+            Assert.AreEqual(r, new[] {"anacell", "betacellular"});
+        }
+
+
+        [Test]
+        public void Example2()
+        {
+            var s = new SolutionIQ542597();
+            var r = s.Solve(2, new[] {"anacell", "betacellular", "cetracular", "deltacellular", "eurocell"}, new[]
+            {
+                "I love anacell Best services; Best services provided by anacell",
+                "betacellular has great services",
+                "deltacellular provides much better services than betacellular",
+                "cetracular is worse than anacell",
+                "Betacellular is better than deltacellular.",
+            });
+            Console.WriteLine(string.Join(" ", r));
+            Assert.AreEqual(r, new[] {"betacellular", "anacell"});
+        }
+    }
+}
