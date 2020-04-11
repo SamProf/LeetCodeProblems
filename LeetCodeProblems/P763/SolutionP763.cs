37b2ef25cae4f2240235f71f95b562abe6afc7a5
SamProf <wsamprof@gmail.com>
2020-04-11 22:13:24 +0200

    P763

---
+++ LeetCodeProblems/P763/SolutionP763.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace LeetCodeProblems.P763
+{
+    public class SolutionP763
+    {
+        public IList<int> PartitionLabels(string S)
+        {
+            var ranges = new List<int>();
+            var symbolsPos = new int['z' - 'a' + 1];
+            for (int i = 0; i < S.Length; i++)
+            {
+                var code = S[i] - 'a';
+                var charPos = symbolsPos[code];
+                if (charPos > 0)
+                {
+                    var rangeIndex = ranges.BinarySearch(charPos - 1);
+
+                    if (rangeIndex < 0)
+                    {
+                        rangeIndex = (~rangeIndex) - 1;
+                    }
+
+                    if (rangeIndex + 1 < ranges.Count)
+                    {
+                        ranges.RemoveRange(rangeIndex + 1, ranges.Count() - rangeIndex - 1);
+                    }
+                }
+                else
+                {
+                    symbolsPos[code] = i + 1;
+                    ranges.Add(i);
+                }
+            }
+
+
+            ranges.RemoveAt(0);
+            ranges.Add(S.Length);
+            var prev = 0;
+            var result = new List<int>();
+            foreach (var range in ranges)
+            {
+                result.Add(range - prev);
+                prev = range;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeProblems/P763/TestP763.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace LeetCodeProblems.P763
+{
+    [TestFixture]
+    public class TestP763
+    {
+        [TestCaseSource(typeof(TestP763), nameof(TestCases))]
+        public void Test(string S, IList<int> expected)
+        {
+            var s = new SolutionP763();
+            var r = s.PartitionLabels(S);
+            Console.WriteLine(string.Join(" ", r));
+            Assert.AreEqual(expected, r);
+        }
+
+        static IEnumerable<object[]> TestCases()
+        {
+            yield return new object[]
+            {
+                "ababcbacadefegdehijhklij",
+                new List<int>() {9, 7, 8},
+            };
+        }
+    }
+}
