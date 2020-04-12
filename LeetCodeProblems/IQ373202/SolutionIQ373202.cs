58d39fcce76aadffa225798548c7807d78df429c
SamProf <wsamprof@gmail.com>
2020-04-12 21:22:42 +0200

    IQ373202

---
+++ LeetCodeProblems/IQ373202/SolutionIQ373202.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeProblems.IQ373202
+{
+    public class SolutionIQ373202
+    {
+        public List<int[]> GetPairs(List<int[]> a, List<int[]> b, int target)
+        {
+            var result = new List<int[]>();
+            a.Sort(Comparer<int[]>.Create((v1, v2) => v1[1].CompareTo(v2[1])));
+            b.Sort(Comparer<int[]>.Create((v1, v2) => v1[1].CompareTo(v2[1])));
+            var ia = 0;
+            var ib = b.Count - 1;
+            
+            var ial = -1; 
+            var ibl = -1;
+            var cl = int.MinValue;
+            while (ia < a.Count && ib >= 0)
+            {
+                var c = a[ia][1] + b[ib][1];
+                if (c > target)
+                {
+                    ib--;
+                }
+                else if (c < target)
+                {
+                    if (cl < c)
+                    {
+                        cl = c;
+                        ial = ia;
+                        ibl = ib;
+                    }
+                    ia++;
+                    
+                }
+                else
+                {
+                    result.Add(new []{a[ia][0], b[ib][0]});
+                    ib--;
+                    ia++;
+                }
+            }
+
+
+            if (result.Count == 0)
+            {
+                if (ial != -1)
+                {
+                    result.Add(new []{a[ial][0], b[ibl][0]});
+                }
+            }
+            
+            return result;
+        }
+    }
+}
---
+++ LeetCodeProblems/IQ373202/TestIQ373202.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace LeetCodeProblems.IQ373202
+{
+    [TestFixture]
+    public class TestIQ373202
+    {
+        [TestCaseSource(typeof(TestIQ373202), nameof(TestCases))]
+        public void Test(List<int[]> a, List<int[]> b, int target, List<int[]> expected)
+        {
+            var s = new SolutionIQ373202();
+            var r = s.GetPairs(a, b, target);
+            Console.WriteLine($"[{string.Join(", ", r.Select(i => $"[{string.Join(", ", i)}]"))}]");
+            Assert.AreEqual(expected, r);
+        }
+
+
+        static IEnumerable<object[]> TestCases()
+        {
+            yield return new object[]
+            {
+                new List<int[]>()
+                {
+                    new int[] {1, 2},
+                    new int[] {2, 4},
+                    new int[] {3, 6},
+                },
+                new List<int[]>()
+                {
+                    new int[] {1, 2},
+                },
+                7,
+                new List<int[]>()
+                {
+                    new int[] {2, 1},
+                },
+            };
+
+            yield return new object[]
+            {
+                new List<int[]>()
+                {
+                    new int[] {1, 3},
+                    new int[] {2, 5},
+                    new int[] {3, 7},
+                    new int[] {4, 10},
+                },
+                new List<int[]>()
+                {
+                    new int[] {1, 2},
+                    new int[] {2, 3},
+                    new int[] {3, 4},
+                    new int[] {4, 5},
+                },
+                10,
+                new List<int[]>()
+                {
+                    new int[] {2, 4},
+                    new int[] {3, 2},
+                },
+            };
+
+
+            yield return new object[]
+            {
+                new List<int[]>()
+                {
+                    new int[] {1, 8},
+                    new int[] {2, 7},
+                    new int[] {3, 14},
+                },
+                new List<int[]>()
+                {
+                    new int[] {1, 5},
+                    new int[] {2, 10},
+                    new int[] {3, 14},
+                },
+                20,
+                new List<int[]>()
+                {
+                    new int[] {3, 1},
+                },
+            };
+
+            yield return new object[]
+            {
+                new List<int[]>()
+                {
+                    new int[] {1, 8},
+                    new int[] {2, 15},
+                    new int[] {3, 9},
+                },
+                new List<int[]>()
+                {
+                    new int[] {1, 8},
+                    new int[] {2, 11},
+                    new int[] {3, 12},
+                },
+                20,
+                new List<int[]>()
+                {
+                    new int[] {1, 3},
+                    new int[] {3, 2},
+                },
+            };
+        }
+    }
+}
