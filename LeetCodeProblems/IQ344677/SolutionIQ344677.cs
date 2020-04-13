899bebf6d9a0aea8231ddfbbba713340f7984e40
SamProf <wsamprof@gmail.com>
2020-04-13 12:16:51 +0200

    IQ344677

---
+++ LeetCodeProblems/IQ344677/SolutionIQ344677.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCodeProblems.IQ344677
+{
+    public class SolutionIQ344677
+    {
+        private bool GetMin(List<int> ropes, List<int> ropes2, ref int i, ref int i2, out int value)
+        {
+            if (i < ropes.Count)
+            {
+                if (i2 < ropes2.Count)
+                {
+                    if (ropes[i] < ropes2[i2])
+                    {
+                        value = ropes[i];
+                        i++;
+                        return true;
+                    }
+                    else
+                    {
+                        value = ropes2[i2];
+                        i2++;
+                        return true;
+                    }
+                }
+                else
+                {
+                    value = ropes[i];
+                    i++;
+                    return true;
+                }
+            }
+            else
+            {
+                if (i2 < ropes2.Count)
+                {
+                    value = ropes2[i2];
+                    i2++;
+                    return true;
+                }
+                else
+                {
+                    value = default(int);
+                    return false;
+                }
+            }
+        }
+
+        public int MinCost(List<int> ropes)
+        {
+            ropes.Sort();
+            var ropes2 = new List<int>();
+            var i = 0;
+            var i2 = 0;
+            var result = 0;
+            while (true)
+            {
+                int v1;
+                int v2;
+                if (!GetMin(ropes, ropes2, ref i, ref i2, out v1))
+                {
+                    break;
+                }
+
+                if (!GetMin(ropes, ropes2, ref i, ref i2, out v2))
+                {
+                    break;
+                }
+
+
+                var v3 = v1 + v2;
+                result += v3;
+                if (i < ropes.Count && v3 <= ropes[i])
+                {
+                    i--;
+                    ropes[i] = v3;
+                }
+                else
+                {
+                    ropes2.Add(v3);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeProblems/IQ344677/TestIQ344677.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace LeetCodeProblems.IQ344677
+{
+    [TestFixture]
+    public class TestIQ344677
+    {
+        [TestCaseSource(typeof(TestIQ344677), nameof(TestCases))]
+        public void Test(List<int> ropes, int expected)
+        {
+            var s = new SolutionIQ344677();
+            var r = s.MinCost(ropes);
+            Console.WriteLine(r);
+            Assert.AreEqual(expected, r);
+        }
+
+
+        static IEnumerable<object[]> TestCases()
+        {
+            yield return new object[]
+            {
+                new List<int>() {8, 4, 6, 12},
+                58
+            };
+
+            yield return new object[]
+            {
+                new List<int>() {20, 4, 8, 2},
+                54
+            };
+
+            yield return new object[]
+            {
+                new List<int>() {1, 2, 5, 10, 35, 89},
+                224
+            };
+
+            yield return new object[]
+            {
+                new List<int>() {2, 2, 3, 3},
+                20
+            };
+        }
+    }
+}
