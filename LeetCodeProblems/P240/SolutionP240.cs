afdf336d1e3d232ac49d0d667485d3efc98e4407
SamProf <wsamprof@gmail.com>
2020-04-16 22:36:49 +0200

    P240

---
+++ LeetCodeProblems/P240/SolutionP240.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LeetCodeProblems.P240
+{
+    public class SolutionP240
+    {
+        public bool SearchMatrix(int[,] matrix, int target)
+        {
+            var m = matrix.GetLength(0);
+            var n = matrix.GetLength(1);
+            var im = 0;
+            var @in = n - 1;
+            while (im < m && @in >= 0)
+            {
+                var c = matrix[im, @in];
+                if (c == target)
+                {
+                    return true;
+                }
+
+                if (target < c)
+                {
+                    @in--;
+                }
+                else
+                {
+                    im++;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCodeProblems/P240/TestP240.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace LeetCodeProblems.P240
+{
+    [TestFixture]
+    public class TestP240
+    {
+        [TestCaseSource(typeof(TestP240), nameof(TestCases))]
+        public void Test(int[,] matrix, int target, bool expected)
+        {
+            var s = new SolutionP240();
+            var r = s.SearchMatrix(matrix, target);
+            Console.WriteLine(r);
+            Assert.AreEqual(expected, r);
+        }
+
+        static IEnumerable<object[]> TestCases()
+        {
+            var matrix = new int[,]
+            {
+                {1, 4, 7, 11, 15},
+                {2, 5, 8, 12, 19},
+                {3, 6, 9, 16, 22},
+                {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30}
+            };
+
+            yield return new object[]
+            {
+                matrix,
+                5,
+                true
+            };
+
+            yield return new object[]
+            {
+                matrix,
+                20,
+                false
+            };
+        }
+    }
+}
