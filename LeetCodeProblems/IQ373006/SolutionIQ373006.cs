4f104246d9b57bd2e9d21498594b7e52c09fe0a8
SamProf <wsamprof@gmail.com>
2020-04-16 23:05:05 +0200

    IQ373006

---
+++ LeetCodeProblems/IQ373006/SolutionIQ373006.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCodeProblems.IQ373006
+{
+    public class SolutionIQ373006
+    {
+        public Dictionary<string, List<string>> Solve(Dictionary<string, List<string>> userSongs,
+            Dictionary<string, List<string>> songGenres)
+        {
+            var songs = new Dictionary<string, string>();
+            foreach (var songGenre in songGenres)
+            {
+                foreach (var song in songGenre.Value)
+                {
+                    songs.Add(song, songGenre.Key);
+                }
+            }
+
+
+            var result = new Dictionary<string, List<string>>();
+
+            foreach (var user in userSongs)
+            {
+                var userGenres = new Dictionary<string, int>();
+                foreach (var song in user.Value)
+                {
+                    if (songs.TryGetValue(song, out var genre))
+                    {
+                        int genreCount;
+                        userGenres.TryGetValue(genre, out genreCount);
+                        userGenres[genre] = genreCount + 1;
+                    }
+                }
+
+                var maxCount = 0;
+                var favorite = result[user.Key] = new List<string>();
+                foreach (var userGenre in userGenres)
+                {
+                    if (maxCount == userGenre.Value)
+                    {
+                        favorite.Add(userGenre.Key);
+                    }
+                    else if (maxCount < userGenre.Value)
+                    {
+                        favorite.Clear();
+                        favorite.Add(userGenre.Key);
+                        maxCount = userGenre.Value;
+                    }
+                    
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeProblems/IQ373006/TestIQ373006.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace LeetCodeProblems.IQ373006
+{
+    [TestFixture]
+    public class TestIQ373006
+    {
+        [TestCaseSource(typeof(TestIQ373006), nameof(TestCases))]
+        public void Test(Dictionary<string, List<string>> userSongs,
+            Dictionary<string, List<string>> songGenres, Dictionary<string, List<string>> expected)
+        {
+            var s = new SolutionIQ373006();
+            var r = s.Solve(userSongs, songGenres);
+            Console.WriteLine(string.Join("\r\n", r.Select(u => $"{u.Key} : {string.Join(", ", u.Value)}")));
+            Assert.AreEqual(expected, r);
+        }
+
+
+        static IEnumerable<object[]> TestCases()
+        {
+            yield return new object[]
+            {
+                new Dictionary<string, List<string>>()
+                {
+                    {"David", new List<string>() {"song1", "song2", "song3", "song4", "song8"}},
+                    {"Emma", new List<string>() {"song5", "song6", "song7"}},
+                },
+
+                new Dictionary<string, List<string>>()
+                {
+                    {"Rock", new List<string>() {"song1", "song3"}},
+                    {"Dubstep", new List<string>() {"song7"}},
+                    {"Techno", new List<string>() {"song2", "song4"}},
+                    {"Pop", new List<string>() {"song5", "song6"}},
+                    {"Jazz", new List<string>() {"song8", "song9"}},
+                },
+
+                new Dictionary<string, List<string>>()
+                {
+                    {"David", new List<string>() {"Rock", "Techno"}},
+                    {"Emma", new List<string>() {"Pop"}},
+                },
+            };
+
+
+            yield return new object[]
+            {
+                new Dictionary<string, List<string>>()
+                {
+                    {"David", new List<string>() {"song1", "song2"}},
+                    {"Emma", new List<string>() {"song3", "song4"}},
+                },
+
+                new Dictionary<string, List<string>>()
+                {
+                },
+
+                new Dictionary<string, List<string>>()
+                {
+                    {"David", new List<string>() { }},
+                    {"Emma", new List<string>() { }},
+                },
+            };
+        }
+    }
+}
